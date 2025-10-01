50ff68c0925b4c43bc614736f63770029c01c41d
Shauna Gordon <shauna@shaunagordon.com>
2025-10-01 10:18:33 -0400

    Remove call to time as it's not really needed (phpunit already displays time info) and is erroring on new setups. Also remove db_host and db_port env var setting, since those are now set in the compose file.
    Signed-off-by: Shauna Gordon <shauna@shaunagordon.com>

--- tests/phpunit.sh
+++ tests/phpunit.sh
@@ -5,27 +5,15 @@ PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BASEDIR=`dirname $PWD`
 
 # db
-# try wercker ports
-RDB_HOST="${RDB_HOST=$RETHINKDB_PORT_28015_TCP_ADDR}"
-RDB_PORT="${RDB_PORT=$RETHINKDB_PORT_28015_TCP_PORT}"
-
-# otherwise, use defaults
-RDB_HOST="${RDB_HOST:=127.0.0.1}"
-RDB_PORT="${RDB_PORT:=28015}"
-
 RDB_DB="${RDB_DB=RQL_TEST_`date +%s`}"
-export RDB_HOST
-export RDB_PORT
 export RDB_DB
-
-composer require amphp/socket
 
 # run tests
 php $BASEDIR/tests/TestHelpers/createDb.php
-ASYNC=yes /usr/bin/time -v $BASEDIR/vendor/bin/phpunit -c $PWD/phpunit.xml --colors=always "$@"
+ASYNC=yes $BASEDIR/vendor/bin/phpunit -c $PWD/phpunit.xml --colors=always "$@"
 php $BASEDIR/tests/TestHelpers/deleteDb.php
 php $BASEDIR/tests/TestHelpers/createDb.php
-ASYNC=no /usr/bin/time -v $BASEDIR/vendor/bin/phpunit -c $PWD/phpunit.xml --colors=always "$@"
+ASYNC=no $BASEDIR/vendor/bin/phpunit -c $PWD/phpunit.xml --colors=always "$@"
 STATUS=$?
 
 #remove db
